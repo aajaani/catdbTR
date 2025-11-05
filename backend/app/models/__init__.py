eba71f1b4f1130b979b5b0ce5bf5ea71c4110573
Aaron Anders Jaani <aaron.anders.jaani@ut.ee>
2025-11-05 18:42:35 +0200

    added rich to requirements.txt

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,5 +4,10 @@ from app.models.manager import Manager
 from app.models.foster_home import FosterHome
 from app.models.cat_procedure import CatProcedure
 from app.models.task import Task
+from app.models.role import Role, RolePermission
+from app.models.audit_log import AuditLog
+from app.models.user import User
+from app.models.cat_file import CatFile
 
-__all__ = ["Cat", "Colony", "Manager", "FosterHome", "CatProcedure", "Task"]
+
+__all__ = ["Cat", "Colony", "Manager", "FosterHome", "CatProcedure", "Task", "Role", "RolePermission", "AuditLog", "User", "CatFile"]
