e002c7cbf24211e3204668c6ef033a72d9da8ce9
Aaron Anders Jaani <aaron.anders.jaani@ut.ee>
2025-11-16 23:14:02 +0200

    hardcoded db connection in backend - temp solution

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,10 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # CONNECT SQLALCHEMY TO THE DATABASE
-DATABASE_URL = (
-    os.getenv("CATDB_DATABASE_URL")
-    or "postgresql+psycopg://user:password@localhost:5432/catdb"
-)
+DATABASE_URL = ("postgresql+psycopg://user:password@db:5432/catdb")
 engine = create_engine(DATABASE_URL)
 
 # each query creates a new session
