143a132da3750cd056e23156e8c8d85ddda3f895
Aaron Anders Jaani <aaron.anders.jaani@ut.ee>
2025-11-16 21:29:06 +0200

    changed env

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # CONNECT SQLALCHEMY TO THE DATABASE
 DATABASE_URL = (
-    os.getenv("DATABASE_URL")
-    or os.getenv("CATDB_DATABASE_URL")
+    os.getenv("CATDB_DATABASE_URL")
     or "postgresql+psycopg://user:password@localhost:5432/catdb"
 )
 engine = create_engine(DATABASE_URL)
