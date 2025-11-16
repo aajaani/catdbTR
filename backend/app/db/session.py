ba4b502699d35ba07008c6893174d1d8f5ca4428
Aaron Anders Jaani <aaron.anders.jaani@ut.ee>
2025-11-16 21:21:03 +0200

    hopefully pipeline now prefers gitlab variable over soem random bs

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,7 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # CONNECT SQLALCHEMY TO THE DATABASE
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://user:password@localhost:5432/catdb")
+DATABASE_URL = (
+    os.getenv("DATABASE_URL")
+    or os.getenv("CATDB_DATABASE_URL")
+    or "postgresql+psycopg://user:password@localhost:5432/catdb"
+)
 engine = create_engine(DATABASE_URL)
 
 # each query creates a new session
